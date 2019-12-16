8de1f850eb5571eb9dd9e0cc6ddd6aa2568f85ec
LindaSind <lin.sindelarova@gmail.com>
2019-12-16 09:17:29 +0100

    audio recorder

--- app/src/main/java/com/example/lsind/ladicka/MainActivity.kt
+++ app/src/main/java/com/example/lsind/ladicka/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.example.lsind.ladicka
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var mediaRecorder: MediaRecorder? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaRecorder = MediaRecorder()
+
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), 0);
+
+        }
+
+
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+
     }
+    fun startRecording(v:View){
+        mediaRecorder?.prepare()
+        mediaRecorder?.start()
+        textView.setText("recording started!")
+    }
+
+    fun stopRecording(v:View){
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+        textView.setText("recording ended!")
+    }
+
 }
+ 
